5f00ecf8227c3e2bcc1f481e8997ff0860d25924
MohammadOwais10 <owais0945@gmail.com>
2021-12-29 21:46:31 +0530

    Finishing Up Cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,99 @@
 import React from 'react';
-import CartItem from './CartItem';
+import Cart from './Cart';
+import Navbar from './Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Cart</h1>
-      <CartItem/>  
-    </div>
-  );
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 99,
+          title: 'Watch',
+          qty: 1,
+          img: 'https://images.unsplash.com/photo-1539874754764-5a96559165b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdhdGNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+          id: 1
+        },
+        {
+          price: 999,
+          title: 'Mobile Phone',
+          qty: 10,
+          img: 'https://images.unsplash.com/photo-1557180295-76eee20ae8aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+          id: 2
+        },
+        {
+          price: 999,
+          title: 'Laptop',
+          qty: 4,
+          img: 'https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGxhcHRvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+          id: 3
+        }
+      ]
+    }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
+  }
+  handleIncreaseQuantity=(product)=>{
+      console.log('Heyy please inc the qty of',product);
+      const{products}=this.state;
+      const index =products.indexOf(product);
+
+      products[index].qty+=1;
+      this.setState({
+          products
+      })
+  }
+  handleDecreaseQuantity=(product)=>{
+    console.log('Heyy please inc the qty of',product);
+    const{products}=this.state;
+    const index =products.indexOf(product);
+    if(products[index].qty===0){
+      return;
+    }
+    products[index].qty -=1;
+    this.setState({
+        products
+    })
+}
+handleDeleteProduct=(id)=>{
+  const { products }=this.state;
+  const items=products.filter((item)=>item.id !== id);
+  this.setState({
+    products:items
+  })
+}
+
+getCartCount =() =>{
+  const {products}=this.state;
+  let count=0;
+  products.forEach((product)=>{
+    count +=product.qty;
+  })
+  return count;
+}
+getCartTotal=()=>{
+  const{products} = this.state;
+  let cartTotal=0;
+  products.map((product)=>{
+    cartTotal=cartTotal+product.qty * product.price
+  })
+  return cartTotal;
+}
+  render() {
+    const {products}=this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()}/>
+        <Cart 
+        products={products}
+        onIncreaseQuantity={this.handleIncreaseQuantity}
+        onDecreaseQuantity={this.handleDecreaseQuantity}
+        onDeleteProduct={this.handleDeleteProduct}
+        />
+       <div style={ {padding: 10, fontSize: 20} }>TOTAL: {this.getCartTotal()} </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,49 +1,50 @@
 import React from "react";
 
-class CartItem extends React.Component{
-    constructor () {
-        super ();
-        this.state={
-            price:990,
-            title:'Mobile',
-            qty :1,
-            img: ' '
-        }
+//class CartItem extends React.Component{
+    // constructor () {
+    //     super ();
+    //     this.state={
+    //         price:990,
+    //         title:'Mobile',
+    //         qty :1,
+    //         img: ' '
+    //     }
         // /*second way to bind*/ this.increaseQuantity=this.increaseQuantity.bind(this);
-    }
-    increaseQuantity =()=> {    /*  finaly way to bind by arrow =()=>  */
+   // }
+  //  increaseQuantity =()=> {    /*  finaly way to bind by arrow =()=>  */
        //console.log('test',this.state);
         // 1st ways--> setState increase qty
         // this.setState({
         //     qty:this.state.qty +1
         // });
          // 2nd ways--> setState increase qty ----if prevState required use this way
-        this.setState((prevState)=>{
-            return{
-            qty:prevState.qty +1
-            }
-        });  
-    }
+    //     this.setState((prevState)=>{
+    //         return{
+    //         qty:prevState.qty +1
+    //         }
+    //     });  
+    // }
 
-    decreaseQuantity =()=> {
-        const { qty }=this.state;
-        if(qty===0){
-            return;
-        }
-         this.setState((prevState)=>{
-             return{
-             qty:prevState.qty -1
-             }
-         });  
-     }
+    // decreaseQuantity =()=> {
+    //     const { qty }=this.state;
+    //     if(qty===0){
+    //         return;
+    //     }
+    //      this.setState((prevState)=>{
+    //          return{
+    //          qty:prevState.qty -1
+    //          }
+    //     } );  
+    // }
      
-    render () {
-        const { price,title,qty}=this.state;
+        const CartItem=(props)=>{
+        const { price, title, qty } = props.product;
+        const { product,onIncreaseQuantity,onDecreaseQuantity,onDeleteProduct}=props;
         return (
             <div className="cart-item">
-                <div className="left-block">
-                    <img style={imgStyles.image}/>
-                </div>
+            <div className="left-block">
+              <img style={styles.image} src={product.img} />
+            </div>
                 <div className="right-block">
                     <div style={{fontSize:25 }}>{title}</div>
                     <div style={{color: '#777' }}>Rs {price}</div>
@@ -53,23 +54,26 @@ class CartItem extends React.Component{
                         <img alt="increase" 
                         className="action-icons" 
                         src="https://cdn-icons.flaticon.com/png/512/1377/premium/1377215.png?token=exp=1639725866~hmac=7d80d3de00920e74cf63449c4ce2b77c"
-                        onClick={this.increaseQuantity}
-                        // /*first way to bind*/ onClick={this.increaseQuantity.bind(this)}
+                        onClick={()=>onIncreaseQuantity(product)}
+                       // /*first way to bind*/ onClick={this.increaseQuantity.bind(this)}
                         />
                         <img alt="dcrease" 
                         className="action-icons" 
                         src="https://cdn-icons.flaticon.com/png/512/3876/premium/3876054.png?token=exp=1639725735~hmac=4293342cbd31e36c35a8ddd7eb1ba1bc"
-                        onClick={this.decreaseQuantity}
+                        onClick={()=>onDecreaseQuantity(product)}
                         />
-                        <img alt="delete" className="action-icons" src="https://cdn-icons.flaticon.com/png/512/3687/premium/3687412.png?token=exp=1639725936~hmac=6bf5f80081951e1f4ca3a147a82d6e94"/>
+                        <img alt="delete" 
+                        className="action-icons" 
+                        src="https://cdn-icons.flaticon.com/png/512/3687/premium/3687412.png?token=exp=1639725936~hmac=6bf5f80081951e1f4ca3a147a82d6e94"
+                        onClick={()=>onDeleteProduct(product.id)}
+                        />
                     </div>
                 </div>
             </div>
         );
     }
-}
 
-const imgStyles={
+const styles={
     image:{
         height:110,
         width:110,
