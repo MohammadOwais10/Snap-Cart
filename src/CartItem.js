faf1d76c5ecbde80ba6023c0dd0ee20034074d8b
MohammadOwais10 <owais0945@gmail.com>
2022-06-04 23:58:43 +0530

    Add Icon

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,85 +1,90 @@
 import React from "react";
 
 //class CartItem extends React.Component{
-    // constructor () {
-    //     super ();
-    //     this.state={
-    //         price:990,
-    //         title:'Mobile',
-    //         qty :1,
-    //         img: ' '
-    //     }
-        // /*second way to bind*/ this.increaseQuantity=this.increaseQuantity.bind(this);
-   // }
-  //  increaseQuantity =()=> {    /*  finaly way to bind by arrow =()=>  */
-       //console.log('test',this.state);
-        // 1st ways--> setState increase qty
-        // this.setState({
-        //     qty:this.state.qty +1
-        // });
-         // 2nd ways--> setState increase qty ----if prevState required use this way
-    //     this.setState((prevState)=>{
-    //         return{
-    //         qty:prevState.qty +1
-    //         }
-    //     });  
-    // }
+// constructor () {
+//     super ();
+//     this.state={
+//         price:990,
+//         title:'Mobile',
+//         qty :1,
+//         img: ' '
+//     }
+// /*second way to bind*/ this.increaseQuantity=this.increaseQuantity.bind(this);
+// }
+//  increaseQuantity =()=> {    /*  finaly way to bind by arrow =()=>  */
+//console.log('test',this.state);
+// 1st ways--> setState increase qty
+// this.setState({
+//     qty:this.state.qty +1
+// });
+// 2nd ways--> setState increase qty ----if prevState required use this way
+//     this.setState((prevState)=>{
+//         return{
+//         qty:prevState.qty +1
+//         }
+//     });
+// }
 
-    // decreaseQuantity =()=> {
-    //     const { qty }=this.state;
-    //     if(qty===0){
-    //         return;
-    //     }
-    //      this.setState((prevState)=>{
-    //          return{
-    //          qty:prevState.qty -1
-    //          }
-    //     } );  
-    // }
-     
-        const CartItem=(props)=>{
-        const { price, title, qty } = props.product;
-        const { product,onIncreaseQuantity,onDecreaseQuantity,onDeleteProduct}=props;
-        return (
-            <div className="cart-item">
-            <div className="left-block">
-              <img style={styles.image} src={product.img} />
-            </div>
-                <div className="right-block">
-                    <div style={{fontSize:25 }}>{title}</div>
-                    <div style={{color: 'white',fontWeight:'bolder' }}>Rs {price}</div>
-                    <div style={{color: '#777',fontWeight:'bolder'}}>Qty: {qty}</div>
-                    <div className="cart-item-actions">
-                        {/*Buttons*/}
-                        <img alt="increase" 
-                        className="action-icons" 
-                        src="https://cdn-icons.flaticon.com/png/512/3876/premium/3876052.png?token=exp=1642699762~hmac=3db1973cd69a29928792c5a3d0c6d52e"
-                        onClick={()=>onIncreaseQuantity(product)}
-                       // /*first way to bind*/ onClick={this.increaseQuantity.bind(this)}
-                        />
-                        <img alt="dcrease" 
-                        className="action-icons" 
-                        src="https://cdn-icons.flaticon.com/png/512/3876/premium/3876054.png?token=exp=1642699645~hmac=d9acf55b8557c922a36c1126543c07ae"
-                        onClick={()=>onDecreaseQuantity(product)}
-                        />
-                        <img alt="delete" 
-                        className="action-icons" 
-                        src="https://cdn-icons.flaticon.com/png/512/6096/premium/6096937.png?token=exp=1642699792~hmac=0d6773481e0180dbe26c696c09ff1c00"
-                        onClick={()=>onDeleteProduct(product.id)}
-                        />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+// decreaseQuantity =()=> {
+//     const { qty }=this.state;
+//     if(qty===0){
+//         return;
+//     }
+//      this.setState((prevState)=>{
+//          return{
+//          qty:prevState.qty -1
+//          }
+//     } );
+// }
 
-const styles={
-    image:{
-        height:110,
-        width:110,
-        borderRadius:4,
-        background:'#ccc'
-    }
-}
+const CartItem = (props) => {
+  const { price, title, qty } = props.product;
+  const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } =
+    props;
+  return (
+    <div className="cart-item">
+      <div className="left-block">
+        <img style={styles.image} src={product.img} />
+      </div>
+      <div className="right-block">
+        <div style={{ fontSize: 25 }}>{title}</div>
+        <div style={{ color: "white", fontWeight: "bolder" }}>Rs {price}</div>
+        <div style={{ color: "#777", fontWeight: "bolder" }}>Qty: {qty}</div>
+        <div className="cart-item-actions">
+          {/*Buttons*/}
+          <img
+            alt="increase"
+            className="action-icons"
+            src="https://cdn-icons-png.flaticon.com/512/1828/1828817.png"
+            onClick={() => onIncreaseQuantity(product)}
+            // /*first way to bind*/ onClick={this.increaseQuantity.bind(this)}
+          />
+          <i class="fa-solid fa-minus"></i>
+          <img
+            alt="dcrease"
+            className="action-icons"
+            src="https://cdn-icons-png.flaticon.com/128/561/561240.png"
+            onClick={() => onDecreaseQuantity(product)}
+          />
+          <img
+            alt="delete"
+            className="action-icons"
+            src="https://cdn-icons-png.flaticon.com/128/3963/3963933.png"
+            onClick={() => onDeleteProduct(product.id)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+};
 
 export default CartItem;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,42 +1,46 @@
 import React from "react";
 
-const Navbar = (props) =>{
-            return ( 
-              <div style={styles.nav}>
-                <h1 style={{font:20,color:"yellow",textAlign:"center"}}>Cart</h1>
-                <div style={styles.cartIconContainer}>
-                    <img style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/512/891/891462.png" alt="cart-icon"/>
-                    <span style={styles.cartCount}>{props.count}</span>
-                </div>
-            </div>
-        );
-    }
+const Navbar = (props) => {
+  return (
+    <div style={styles.nav}>
+      <h1 style={{ font: 20, color: "yellow", textAlign: "center" }}>Cart</h1>
+      <div style={styles.cartIconContainer}>
+        <img
+          style={styles.cartIcon}
+          src="https://cdn-icons-png.flaticon.com/128/3081/3081648.png"
+          alt="cart-icon"
+        />
+        <span style={styles.cartCount}>{props.count}</span>
+      </div>
+    </div>
+  );
+};
 
 const styles = {
-    cartIcon: {
-      height: 32,
-      marginRight: 20
-    },
-    nav: {
-      height: 70,
-      background: 'linear-gradient(to right, #8e2de2, #4a00e0)',
-      display: 'flex',
-      justifyContent: 'center',
-      //justifyContent: 'flex-end',
-      alignItems: 'center',
-      padding:10,
-    },
-    cartIconContainer: {
-      position: 'relative'
-    },
-    cartCount: {
-      background: 'yellow',
-      borderRadius: '50%',
-      padding: '4px 8px',
-      position: 'absolute',
-      right: 0,
-      top: -9
-    }
-  };
+  cartIcon: {
+    height: 32,
+    marginRight: 20,
+  },
+  nav: {
+    height: 70,
+    background: "linear-gradient(to right, #8e2de2, #4a00e0)",
+    display: "flex",
+    justifyContent: "center",
+    //justifyContent: 'flex-end',
+    alignItems: "center",
+    padding: 10,
+  },
+  cartIconContainer: {
+    position: "relative",
+  },
+  cartCount: {
+    background: "yellow",
+    borderRadius: "50%",
+    padding: "4px 8px",
+    position: "absolute",
+    right: 0,
+    top: -9,
+  },
+};
 
 export default Navbar;
